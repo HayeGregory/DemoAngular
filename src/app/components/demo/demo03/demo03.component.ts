013dfecdb1a3970382f03be7999f1c1192fbc89a
HayeGregory <haye.greg@outlook.com>
2021-10-01 02:08:22 +0200

    demo03 - Event Binding

--- src/app/components/demo/demo03/demo03.component.ts
+++ src/app/components/demo/demo03/demo03.component.ts
@@ -3,13 +3,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-demo03',
   templateUrl: './demo03.component.html',
-  styleUrls: ['./demo03.component.scss']
+  styleUrls: ['./demo03.component.scss'],
 })
 export class Demo03Component implements OnInit {
+  maVariable: String = '';
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.maVariable = 'salut tout me monde!';
   }
 
+  maMethode() {
+    this.maVariable = 'on change la valeur';
+  }
 }
